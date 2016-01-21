7a94c2d3b63737754a221fcc2ba30c0826b3b9ff
Wei Luo <rovluo@gmail.com>
2016-01-21 10:10:01 +0800

    1.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 setup(
   name='delivery_qos',
-  version='1.0.2',
+  version='1.0.3',
   author='rovere',
   author_email='luowei@mgtv.com',
   url = "https://github.com/cultcode/delivery_qos",
