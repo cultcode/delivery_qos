a380532c0ce6c45c239fd21abb11871766d0bb0e
Wei Luo <rovluo@gmail.com>
2016-01-21 10:09:04 +0800

    add data_files

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setup(
   url = "https://github.com/cultcode/delivery_qos",
   packages=['delivery_qos'],
   #install_requires=['scandir'],
+  data_files=[('/etc/cron.d', ['data/etc/cron.d/delivery_scan']),
+              ('/etc', ['data/etc/delivery_scan.json'])
+             ],
   entry_points={
     "console_scripts":[
       '''delivery_scan = delivery_qos.scan:scan''',
